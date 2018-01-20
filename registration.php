325f0cebe59c35394a0fbe6af2b9d4f5d7516aa8
Athanasios Galanis on iMac271 <ath.galanis@mycenter.gr>
2018-01-20 23:20:48 +0200

    Create empty module

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Copyright © 2017 gladd. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Gladd_Demo',
+    __DIR__
+);
